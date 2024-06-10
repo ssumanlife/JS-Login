cbf2115a88c6de9360f39019b3f42955074d4d4b
ssumanlife <ksm9805@naver.com>
2024-06-10 16:08:45 +0900

    로직 수정

--- login/main.js
+++ login/main.js
@@ -9,12 +9,8 @@ const infoList = {
 }
 
 function login() {
-  // const loginId = document.querySelector(".idInput")
-  // const password = document.querySelector(".passwordInput")
-
-  let a = loginId.value
-  if (infoList[a] != undefined && infoList[a] != null) {
-    if (infoList[a] === password.value) {
+  if (infoList[loginId.value] != undefined && infoList[loginId.value] != null) {
+    if (infoList[loginId.value] === password.value) {
       alert(`환영합니다. ${loginId.value}님!`)
     } else {
       alert("비밀번호를 확인해주세요.")
@@ -24,6 +20,4 @@ function login() {
   }
 }
 
-// js파일이 읽혀내려가는 순간 login()가 실행되는 것을 막기위해 콜백함수를 핸들러로 적용
-// 콜백함수는 특정한 실행 위치를 보장해주는 용도로 활용!!
-btn.addEventListener("click", () => login())
+btn.addEventListener("click", login)
